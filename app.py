96079db2ac1d8212b27a8cf8278d3a4394827f45
Swierczewski Michal <michal.swierczewski@nokia.com>
2020-11-30 10:03:50 +0100

    test

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+from flask import Response
+
+import requests
+
+from checkStatus import get_status_code
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print("hello world!")
-    return "svc app works!"
+URL = "http://localhost:5000"
 
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+@app.route('/', methods=['GET'])
+def home():
+    return "hello world\n"
+
+@app.route('/healthz', methods=['GET'])
+def get():
+    """health check"""
+    
+    # NO verification
+    # return "OK", 200
+
+    # WITH verification
+    if get_status_code(URL) == 200:
+        return Response("ok", status = 200)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', debug = False)
